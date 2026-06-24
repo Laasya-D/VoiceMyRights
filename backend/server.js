ef6636e94865b1071e34f17a82a9426ff7d89fc0
Bhargavi <bhargavidorle25@gmail.com>
2026-06-24 22:52:59 +0530

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("VoiceMyRights backend running!");
+});
+
+app.post("/analyze", (req, res) => {
+  res.json({
+    score: 68,
+    concerns: [
+      "Data sharing with third parties",
+      "Terms can change anytime"
+    ]
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
